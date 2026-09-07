8e4929e87c3ad66b3b4e1a90fbf10d9c407d98a7
Thomzzen <48518595+Thomzzen@users.noreply.github.com>
2026-09-07 21:45:51 +0200

    Add labels.js: the managed list of filter labels
    Pulls the homepage's filter pills out of "whatever tags happen to be
    used" and into an explicit, editable list, managed from the new
    "Filter labels" panel in admin.html.

---
+++ blog/labels.js
@@ -0,0 +1,14 @@
+/*
+ * LABELS is the managed list of filter labels shown as pills on the
+ * homepage, and offered as the "Tag" dropdown for each gadget in
+ * admin.html.
+ *
+ * Manage this from admin.html's "Filter labels" panel (add/remove there
+ * publishes straight to this file), or edit the array by hand and push.
+ */
+const LABELS = [
+  "Editor's Pick",
+  "Trending",
+  "Budget Pick",
+  "Splurge"
+];
